0ca7f72c3bd05251e986681be81c6b8f368646b8
Vachaspati Annaldas <annaldasvachaspati@gmail.com>
2026-05-23 22:25:02 +0530

    Add vulnerable SQL query

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def get_user(user_id):
 
-    query = """
-        SELECT * FROM users
-        WHERE id = %s
-    """
+    query = (
+        "SELECT * FROM users WHERE id="
+        + user_id
+    )
 
     return query
